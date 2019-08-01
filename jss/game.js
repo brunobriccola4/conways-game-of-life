6af6a677a9514c110541067defd5a7586b6be6eb
Bruno <brunobriccola4@gmail.com>
2019-08-01 20:58:51 -0300

    Add new Play/Stop and SaveGame buttons

--- jss/game.js
+++ jss/game.js
@@ -3,12 +3,15 @@ var Game = {
 	play: null,
 	board: null,
 	cells: null,
+	save: null,
+	interval:null,
 	init: function () {
 		Board.createCells();
 		Game.Board = Board.getBoard();
 		Game.cells = Board.getCells();
 		Game.next = document.getElementById('next');
 		Game.play = document.getElementById('play');
+		Game.save = document.getElementById('save');
 	},
 	start: function () {
 		for (var i = 0; i < Game.cells.length; i++){
@@ -16,8 +19,18 @@ var Game = {
 		}
 		Game.next.onclick = Board.nextStep;
 		Game.play.onclick = Game.update;
+
+		
 	},
 	update: function () {
-		setInterval(Board.nextStep, 1000);
+		if (Game.play.innerHTML === 'Play') {
+			Game.interval = setInterval(Board.nextStep, 1000);
+			Game.play.innerHTML = 'Stop';
+			Game.next.disabled = true;
+		  } else {
+			clearInterval(Game.interval);
+			Game.play.innerHTML = 'Play';
+			Game.next.disabled = false;
+		  }
 	}
 };
