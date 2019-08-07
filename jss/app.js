bfa4e1d8f3977cf02e2105332b6509b391cdc8ab
Bruno <brunobriccola4@gmail.com>
2019-08-07 16:41:54 -0300

    refactored files

--- jss/app.js
+++ jss/app.js
@@ -1,16 +1,19 @@
-var Play = function () {
+var Play = function() {
 	var liAlives = Board.getCells();
 	Board.updateCells(liAlives)
 }
 
-var init = function () {
-
-	Game.init();
-	Game.start();
-	//var btnPlay = Board.getPlayButton();
-	//btnPlay.onclick = play;
+var init = function() {
+	if(localStorage.getItem('game' != null)) 
+	{
+		Window.hideModalWindow();
+		storage.loadWindow();
+	} else {
+		storage.hideLoad();
+		Window.Accept()
+		Window.InputFocus();
+	}
 }
-
 window.onload = init; 
 
 
